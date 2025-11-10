cc68ca115d27cc9fef83d9b2c2c81f725f30df89
JL-Huawei_Graznov <graznov@me.com>
2025-11-10 08:24:15 +0300

    10.11.25

--- src/main.js
+++ src/main.js
@@ -1,9 +1,30 @@
 import './style.css'
 
+let bool = true
+function clickBtn(){
+    
+}
+
+
+
 document.querySelector('#app').innerHTML = `
+
   <div>
         <h1>my Dentist</h1>
+        
+        
+        <button>
+           
+        </button>
+
+        
   </div>
+  
 `
 
-setupCounter(document.querySelector('#counter'))
+
+document.querySelector('btn').addEventListener('click', click(bool));
+
+
+
+
