57f2f1c5e15530fe4b9086313b6a1859dcc53def
JL-Huawei_Graznov <graznov@me.com>
2025-11-12 12:14:47 +0300

    12.11.25

--- src/main.js
+++ src/main.js
@@ -1,26 +1,44 @@
 import './style.css'
-import {RESURSES} from "../resurs.js";
+import {RESURSES} from "./resurs.js";
 
-let bool = true
-function clickBtn(){
-    
-}
+document.querySelector('.my').innerHTML = `
 
-// const vallue = RESURSES.VALLUE
-
-
-
-document.querySelector('#app').innerHTML = `
-
-  <div>
-        <h1>my Dentist</h1>
+  <div class="container">
+  
+    <header>
+        <div class="content">
         
-        <p>${RESURSES.VALLUE}</p>
- 
-         
-        
+            <div class="top_menu_head">
+                <ul>
+                    <li><a href="">about me</a></li>
+                    <li><a href="">my works</a></li>
+                    <li><a href="">reviews from my patients</a></li>
+                    <li><a href="">locatoin</a></li>
+                    <li>8 (000) 000 00 00 </li>
+                </ul>
+              
+            </div>
+            
+<!--            <div class="bootom">-->
+                <div class="main_head">
+                    <div class="main_head_left">
+                    
+                    
+                        <p>${RESURSES.VALLUE}</p>
+                    </div>
+                    
+                    <div class="main_head_right">
+                        <img width="500px" class="main_head_right_img" src="../public/Doctor_Female.png" alt="">
+                    </div>
+<!--                </div>            -->
+            </div>
+            
 
-        
+            
+            
+        </div>       
+    </header>
+  
   </div>
   
 `
