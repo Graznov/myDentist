e2e1a8248f5c1842f5d213f416c4200bbd420146
JL-Huawei_Graznov <graznov@me.com>
2025-11-15 01:16:12 +0300

    15.11.25

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ![text](public/Doctor_Female.png)
 
+
+# 14.11.25 main->o moey rabote, moi raboti
 # 13.11.25 - header bacground
 # 13.11.25 header
 # 09.11.25 Начало
--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ function animateTextElements() {
     const elements = [
         document.querySelector('.text_one'),
         document.querySelector('.text_two'),
-        document.querySelector('.text_three')
+        document.querySelector('.text_three'),
+        document.querySelector('.zagolovok'),
+        document.querySelector('.text')
     ];
 
     // Последовательно добавляем класс visible с задержкой
@@ -26,6 +28,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Или при полной загрузке страницы
 window.addEventListener('load', animateTextElements);
+
+// //слайдер...
+// const slides = document.querySelector('.slides');
+// const slide = document.querySelectorAll('.slide');
+// let index = 0;
+//
+// document.querySelector('.next').addEventListener('click', () => {
+//     index = (index + 1) % slide.length;
+//     slides.style.transform = `translateX(-${index * 100}%)`;
+// });
+//
+// document.querySelector('.prev').addEventListener('click', () => {
+//     index = (index - 1 + slide.length) % slide.length;
+//     slides.style.transform = `translateX(-${index * 100}%)`;
+// });
+// //...слайдер
 
 document.querySelector('.my').innerHTML = `
 
@@ -66,13 +84,87 @@ document.querySelector('.my').innerHTML = `
                 </div>
             
         </div>     
-        
-          
     </header>
+    <main>
+        <div class="content">
+            <div class="o_moey_rabote">
+            
+                <div class="o_moey_rabote_Left">
+                     <img src="../public/two.png" alt="smiles">
+                </div>
+                
+                <div class="o_moey_rabote_Right">
+                
+                    <div class="zagolovok">
+                        ${RESURSES.O_MOEY_RABOTE.ZAGOLOVOK}
+                    </div>
+                    <div class="text">
+                        <p>${RESURSES.O_MOEY_RABOTE.TEXT}</p>
+                    </div>
+                    
+                </div>
+                
+            </div>
+            
+<div class="moi_raboti">
+    
+    <div class="zagolovok">${RESURSES.SLIDER_TEXT}</div>
+
+    <div class="slider">
+      <div class="slides">
+                <img src="../public/smile/img1.jpeg" class="slide" alt="">
+                <img src="../public/smile/img2.jpeg" class="slide" alt="">
+                <img src="../public/smile/img3.jpeg" class="slide" alt="">
+                <img src="../public/smile/img4.jpeg" class="slide" alt="">
+                <img src="../public/smile/img5.jpg" class="slide" alt="">
+      </div>
+    
+      <button class="prev">‹</button>
+      <button class="next">›</button>
+      
+    </div>
+    
+    <div class="slider-dots"></div>
+</div>
+            
+            
+            
+<!--            <div class="o_moey_rabote">-->
+<!--                <div class="o_moey_rabote_Right">-->
+
+<!--                    <div class="zagolovok">${RESURSES.O_MOEY_RABOTE.ZAGOLOVOK}-->
+                        
+<!--                    </div>-->
+<!--                    <div class="text"><p>${RESURSES.O_MOEY_RABOTE.TEXT}</p>-->
+                        
+<!--                    </div>  -->
+<!--                </div>-->
+<!--                <div class="o_moey_rabote_Left">-->
+<!--                     <img src="../public/two.png" alt="smiles">-->
+<!--                </div>-->
+            </div>
+        </div>
+    </main>
   
   </div>
   
 `
+
+//слайдер...
+const slides = document.querySelector('.slides');
+const slide = document.querySelectorAll('.slide');
+let index = 0;
+
+document.querySelector('.next').addEventListener('click', () => {
+    index = (index + 1) % slide.length;
+    slides.style.transform = `translateX(-${index * 100}%)`;
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+    index = (index - 1 + slide.length) % slide.length;
+    slides.style.transform = `translateX(-${index * 100}%)`;
+});
+//...слайдер
 
 
 
